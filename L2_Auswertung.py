27dabd5d83d18049349f08f5e987a0234a7dd9b5
Tristis <henhov112@gmail.com>
2025-12-25 17:59:50 +0100

    general u_A implemented

--- L2_Auswertung.py
+++ L2_Auswertung.py
@@ -5,7 +5,21 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 from scipy.optimize import least_squares
 from scipy.signal import find_peaks
+from scipy.differentiate import derivative
 from scipy import constants
+
+
+# Source - https://stackoverflow.com/a
+# Posted by alko
+# Retrieved 2025-12-25, License - CC BY-SA 3.0
+
+def partial_derivative(func, var=0, point=[]):
+    args = point[:]
+    def wraps(x):
+        args[var] = x
+        return func(*args)
+    return derivative(wraps, point[var], dx = 1e-6)
+
 
 mpl.rcParams['text.usetex'] = True
 mpl.rcParams.update(mpl.rcParamsDefault)
@@ -13,57 +27,42 @@ mpl.rcParams.update(mpl.rcParamsDefault)
 uncertainty_frequency = 2 * constants.pi * 0.225 / (2 * np.sqrt(3))
 uncertainty_amplitude = 0.01 #10 mV
 
-def u_A(a, omega_0, omega, delta, C, u_a, u_omega_0, u_omega, u_delta, u_B, u_C):
-    # this function is the result of a translation from a Latex equation to python expression
-    denom1 = 4 * delta**2 * omega**2 + (omega_0**2 - omega**2)**2
+def u_A(params, u_params):
+    result = 0
+    for i in u_params:
+        res = partial_derivative(fit_func, var = i, point = params)
+        result += np.square(res.df[0] * u_params[i])
+    return np.sqrt(result)
 
-    term_a = (1 / np.sqrt(denom1) * u_a)**2
-
-    term_omega_0 = (
-        (-2 * a * omega_0 * (omega_0**2 - omega**2))
-        / (denom1**(3/2))
-        * u_omega_0
-    )**2
-
-    term_omega = (
-        (-2 * a * omega * (-omega_0**2 + 2 * delta**2 + omega**2))
-        / ((omega_0**4 - 2 * omega_0**2 * omega**2 + 4 * delta**2 * omega**2 + omega**4)**(3/2))
-        * u_omega
-    )**2
-
-    term_delta = (
-        (-4 * a * delta * omega**2)
-        / (denom1**(3/2))
-        * u_delta
-    )**2
-
-    return np.sqrt(term_a + term_omega_0 + term_omega + term_delta + u_B**2)
-
-def fit_func(params:list, omega):
-    a, omega_0, delta, B, C = params[0], params[1], params[2], params[3], params[4]
+def fit_func(params:list):
+    omega, a, omega_0, delta, B, C = params[0], params[1], params[2], params[3], params[4], params[5]
     return a / np.sqrt(np.square(np.square(omega_0) - np.square(omega + C)) + np.square(2 * delta * (omega + C))) + B # = A(\omega)
 
+def fit_func_odr(params:list, omega):
+    params.insert(0, omega)
+    return fit_func(params)
+
 def evaluation(frequency:list, amplitude:list, label, fig, axs):
-    fit_func_instance = odr.Model(fit_func)
+    fit_func_odr = odr.Model(fit_func_odr)
     data = odr.RealData(frequency, amplitude, sx = uncertainty_frequency, sy = uncertainty_amplitude)
-    odr_fit = odr.ODR(data,fit_func_instance, beta0 = [400, 3.3, 0.2, 0, 0])
+    odr_fit = odr.ODR(data,fit_func_odr, beta0 = [400, 3.3, 0.2, 0, 0])
     output = odr_fit.run()
 
     output.pprint()
 
     a, omega_0, delta, B, C = output.beta[0], output.beta[1], output.beta[2], output.beta[3], output.beta[4]
-    u_a, u_omega_0, u_delta, u_B = output.sd_beta[0], output.sd_beta[1], output.sd_beta[2], output.sd_beta[3]
+    u_a, u_omega_0, u_delta, u_B, u_C = output.sd_beta[0], output.sd_beta[1], output.sd_beta[2], output.sd_beta[3], output.sd_beta[4]
 
     x = np.linspace(min(frequency), max(frequency), 10000)
-    y = [fit_func([a, omega_0, delta, B, C], i) for i in x]
+    y = [fit_func_odr([a, omega_0, delta, B, C], i) for i in x]
     axs.plot(x,y, label = f"ODR Fit", c = "cyan")
 
     peaks, props = find_peaks(y)
     resonance_frequency = x[peaks[0]]
     print(resonance_frequency)
 
-    amplitude_res_freq = fit_func([a, omega_0, delta, B, C], resonance_frequency)
-    u_amplitude_res_freq = u_A(a, omega_0, resonance_frequency, delta, u_a, u_omega_0, u_omega_0, u_delta, u_B)
+    amplitude_res_freq = fit_func_odr([a, omega_0, delta, B, C], resonance_frequency)
+    u_amplitude_res_freq = u_A(output.beta, [u_omega_0, u_a, u_delta, u_B, u_C])
 
     axs.errorbar(resonance_frequency, amplitude_res_freq, xerr = u_omega_0, yerr = u_amplitude_res_freq, label = r"Resonanzfrequenz $\omega_0$",c = "blue", fmt = "o", capsize = 3)
 
@@ -75,7 +74,7 @@ def evaluation(frequency:list, amplitude:list, label, fig, axs):
     print(rf"Amplitude at cut off freq: {amplitude_cut_off_freq} {u'\u00b1'} {u_amplitude_cut_off_freq}")
     
     args = (a, omega_0, delta, B, C)
-    func = lambda omega, a, omega_0, delta, B, C: fit_func([a, omega_0, delta, B, C], omega) - amplitude_cut_off_freq
+    func = lambda omega, a, omega_0, delta, B, C: fit_func_odr([a, omega_0, delta, B, C], omega) - amplitude_cut_off_freq
     cut_off_freq_0 = least_squares(func, x0 = 2.5, bounds = (0, resonance_frequency), args = args)["x"][0]
     cut_off_freq_1 = least_squares(func, x0 = resonance_frequency+2, bounds = (resonance_frequency, np.inf), args = args)["x"][0]
 
