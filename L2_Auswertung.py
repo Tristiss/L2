458d6fc7fb92f2d65a0003cc5f63a8430c0141d7
Tristis <henhov112@gmail.com>
2026-01-02 00:19:06 +0100

    all measurement data added, the varibales needed to change the masking / fit guess and horizontal_shift were made easier to change in the main script, mes 3 is not converging properly yet, monte carlo uncertainty propagation needs to be implemented next

--- L2_Auswertung.py
+++ L2_Auswertung.py
@@ -10,25 +10,57 @@ mpl.rcParams['text.usetex'] = True
 mpl.rcParams.update(mpl.rcParamsDefault)
 
 uncertainty_frequency = 2 * constants.pi * 0.225 / (2 * np.sqrt(3))
-uncertainty_amplitude = 0.01 #10 mV
+uncertainty_amplitude = 10 # mV
 
 mes_points = 100
 
-num = 5
-curr = 0.5
+num = 4
 
-lower_bound = 39
-upper_bound = 0
+fit_guesses = {
+    3 : [100, 3.2, 0.2, 0, 0],
+    4 : [200, 3.3, 0.2, 0, 0],
+    5 : [200, 3.2, 0.3, 0],
+    7 : [200, 3.2, 0.3, 0, 0]
+}
 
-fit_guesses = [500, 3.2, 0.3, 0, 0]
+horizontal_shift_dict = {
+    3 : True,
+    4 : True, 
+    5 : False,
+    7 : True
+}
+
+maskings = {
+    3 : [3, 5, 47],
+    4 : [0, 0, 0],
+    5 : [1, 39, 0],
+    7 : [0, 0, 0]
+}
+
+currents = {
+    3 : 0,
+    4 : 0.25,
+    5 : 0.5,
+    7 : 0.75
+}
+
+curr = currents[num]
+
+active_fit_guess = fit_guesses[num]
 
 test_phase = True
 
-mask_type = 1
+horizontal_shift = horizontal_shift_dict[num]
+
+mask_type = maskings[num][0]
 # 0 => no masking
 # 1 => mask only left side
 # 2 => mask only right side
 # 3 => mask both sides
+# 4 => middle
+
+lower_bound = maskings[num][1]
+upper_bound = maskings[num][2]
 
 """
 def partial_derivative(var:int, point:list):
@@ -54,25 +86,33 @@ def main():
     
     match mask_type:
         case 0:
-            evaluation.eval_L2(frequency[lower_bound:upper_bound], amplitude[lower_bound:upper_bound], axs, uncertainty_frequency, uncertainty_amplitude, fit_guesses, test_phase)
-            axs.errorbar(frequency[lower_bound:upper_bound], amplitude[lower_bound:upper_bound], xerr = uncertainty_frequency, yerr = uncertainty_amplitude, label = f"gefittete Messdaten", capsize = 3, c = "orange")
+            evaluation.eval_L2(frequency, amplitude, axs, uncertainty_frequency, uncertainty_amplitude, active_fit_guess, horizontal_shift, test_phase)
+            axs.errorbar(frequency, amplitude, xerr = uncertainty_frequency, yerr = uncertainty_amplitude, label = f"gefittete Messdaten", capsize = 3, c = "orange")
         case 1:
-            evaluation.eval_L2(frequency[lower_bound:], amplitude[lower_bound:], axs, uncertainty_frequency, uncertainty_amplitude, fit_guesses, test_phase)
+            evaluation.eval_L2(frequency[lower_bound:], amplitude[lower_bound:], axs, uncertainty_frequency, uncertainty_amplitude, active_fit_guess, horizontal_shift, test_phase)
             axs.errorbar(frequency[lower_bound:], amplitude[lower_bound:], xerr = uncertainty_frequency, yerr = uncertainty_amplitude, label = f"gefittete Messdaten", capsize = 3, c = "orange")
             axs.errorbar(frequency[:- (mes_points - lower_bound)], amplitude[:- (mes_points - lower_bound)], xerr = uncertainty_frequency, yerr = uncertainty_amplitude, label = f"maskierte Messdaten", capsize = 3, c = "m", fmt = "o")
         case 2:
-            evaluation.eval_L2(frequency[:upper_bound], amplitude[:upper_bound], axs, uncertainty_frequency, uncertainty_amplitude, fit_guesses, test_phase)
+            evaluation.eval_L2(frequency[:upper_bound], amplitude[:upper_bound], axs, uncertainty_frequency, uncertainty_amplitude, active_fit_guess, horizontal_shift, test_phase)
             axs.errorbar(frequency[:upper_bound], amplitude[:upper_bound], xerr = uncertainty_frequency, yerr = uncertainty_amplitude, label = f"gefittete Messdaten", capsize = 3, c = "orange")
             axs.errorbar(frequency[upper_bound:], amplitude[upper_bound:], xerr = uncertainty_frequency, yerr = uncertainty_amplitude, label = f"maskierte Messdaten", capsize = 3, c = "m", fmt = "o")
         case 3:
             masked_data_freq = list(frequency.copy())
             masked_data_amp = list(amplitude.copy())
-            evaluation.eval_L2(frequency[lower_bound:upper_bound], amplitude[lower_bound:upper_bound], axs, uncertainty_frequency, uncertainty_amplitude, fit_guesses, test_phase)
+            evaluation.eval_L2(frequency[lower_bound:upper_bound], amplitude[lower_bound:upper_bound], axs, uncertainty_frequency, uncertainty_amplitude, active_fit_guess, horizontal_shift, test_phase)
             axs.errorbar(frequency[lower_bound:upper_bound], amplitude[lower_bound:upper_bound], xerr = uncertainty_frequency, yerr = uncertainty_amplitude, label = f"gefittete Messdaten", capsize = 3, c = "orange")
             del masked_data_freq[lower_bound:upper_bound]
             del masked_data_amp[lower_bound:upper_bound]
             axs.errorbar(masked_data_freq, masked_data_amp, xerr = uncertainty_frequency, yerr = uncertainty_amplitude, label = f"maskierte Messdaten", capsize = 3, c = "m", fmt = "o")
-    
+        case 4:
+            masked_data_freq = list(frequency.copy())
+            masked_data_amp = list(amplitude.copy())
+            del masked_data_freq[lower_bound:upper_bound]
+            del masked_data_amp[lower_bound:upper_bound]
+            evaluation.eval_L2(masked_data_freq, masked_data_amp, axs, uncertainty_frequency, uncertainty_amplitude, active_fit_guess, horizontal_shift, test_phase)
+            axs.errorbar(masked_data_freq, masked_data_amp, xerr = uncertainty_frequency, yerr = uncertainty_amplitude, label = f"gefittete Messdaten", capsize = 3, c = "orange")
+            axs.errorbar(frequency[lower_bound:upper_bound], amplitude[lower_bound:upper_bound], xerr = uncertainty_frequency, yerr = uncertainty_amplitude, label = f"maskierte Messdaten", capsize = 3, c = "m", fmt = "o")
+
     axs.legend()
     axs.grid()
     axs.set_xlabel(r'Frequenz $f$ [$\text{s}^{-1}$]')
--- functions_for_eval.py
+++ functions_for_eval.py
@@ -44,64 +44,130 @@ class Fit_Functions_L2():
         return np.array(res)
 
     @staticmethod
-    def fit_func_odr(params:list, omega):
+    def fit_func_odr_with_shift(params:list, omega):
         a, omega_0, delta, B, C = params[0], params[1], params[2], params[3], params[4]
         return a / np.sqrt(np.square(np.square(omega_0) - np.square(omega + C)) + np.square(2 * delta * (omega + C))) + B # = A(\omega)
-        #np.insert(params, 0, omega)
-        #return fit_func(params)
+    
+
+    # The next functions are without the horizontal shift
+    @staticmethod
+    def fit_func_om_wo(omega, params:list):
+        a, omega_0, delta, B = params[1], params[2], params[3], params[4]
+        res = a / np.sqrt(np.square(np.square(omega_0) - np.square(omega)) + np.square(2 * delta * (omega))) + B # = A(\omega)
+        return np.array(res)
+
+    @staticmethod
+    def fit_func_a_wo(a, params:list):
+        omega, omega_0, delta, B = params[0], params[2], params[3], params[4]
+        res = a / np.sqrt(np.square(np.square(omega_0) - np.square(omega)) + np.square(2 * delta * (omega))) + B # = A(\omega)
+        return np.array(res)
+
+    @staticmethod
+    def fit_func_om0_wo(omega_0, params:list):
+        omega, a, delta, B = params[0], params[1], params[3], params[4]
+        res = a / np.sqrt(np.square(np.square(omega_0) - np.square(omega)) + np.square(2 * delta * (omega))) + B # = A(\omega)
+        return np.array(res)
+
+    @staticmethod
+    def fit_func_del_wo(delta, params:list):
+        omega, a, omega_0, B = params[0], params[1], params[2], params[4]
+        res = a / np.sqrt(np.square(np.square(omega_0) - np.square(omega)) + np.square(2 * delta * (omega))) + B # = A(\omega)
+        return np.array(res)
+
+    @staticmethod
+    def fit_func_B_wo(B, params:list):
+        omega, a, omega_0, delta = params[0], params[1], params[2], params[3]
+        res = a / np.sqrt(np.square(np.square(omega_0) - np.square(omega)) + np.square(2 * delta * (omega))) + B # = A(\omega)
+        return np.array(res)
+
+    @staticmethod
+    def fit_func_odr_wo_shift(params:list, omega):
+        a, omega_0, delta, B = params[0], params[1], params[2], params[3]
+        return a / np.sqrt(np.square(np.square(omega_0) - np.square(omega)) + np.square(2 * delta * (omega))) + B # = A(\omega)
 
 
 class evaluation():
 
     @staticmethod
-    def u_A(params, u_params):
+    def u_A(params, u_params, horizontal_shift):
         #res = partial_derivative(var = i, point = params)
         result = 0
-        print(params[0])
-        res = derivative(Fit_Functions_L2.fit_func_om, params[0], args = [params])
+        if horizontal_shift == True:
+            res = derivative(Fit_Functions_L2.fit_func_om, params[0], args = [params])
+            result += np.square(res.df[0] * u_params[0])
+
+            res = derivative(Fit_Functions_L2.fit_func_a, params[1], args = [params])
+            result += np.square(res.df[0] * u_params[1])
+            
+            res = derivative(Fit_Functions_L2.fit_func_om0, params[2], args = [params])
+            result += np.square(res.df[0] * u_params[2])
+            
+            res = derivative(Fit_Functions_L2.fit_func_del, params[3], args = [params])
+            result += np.square(res.df[0] * u_params[3])
+
+            res = derivative(Fit_Functions_L2.fit_func_B, params[4], args = [params])
+            result += np.square(res.df[0] * u_params[4])
+            
+            res = derivative(Fit_Functions_L2.fit_func_C, params[5], args = [params])
+            result += np.square(res.df[0] * u_params[5])
+            
+            return np.sqrt(result)
+    
+        res = derivative(Fit_Functions_L2.fit_func_om_wo, params[0], args = [params])
         result += np.square(res.df[0] * u_params[0])
 
-        res = derivative(Fit_Functions_L2.fit_func_a, params[1], args = [params])
+        res = derivative(Fit_Functions_L2.fit_func_a_wo, params[1], args = [params])
         result += np.square(res.df[0] * u_params[1])
         
-        res = derivative(Fit_Functions_L2.fit_func_om0, params[2], args = [params])
+        res = derivative(Fit_Functions_L2.fit_func_om0_wo, params[2], args = [params])
         result += np.square(res.df[0] * u_params[2])
         
-        res = derivative(Fit_Functions_L2.fit_func_del, params[3], args = [params])
+        res = derivative(Fit_Functions_L2.fit_func_del_wo, params[3], args = [params])
         result += np.square(res.df[0] * u_params[3])
 
-        res = derivative(Fit_Functions_L2.fit_func_B, params[4], args = [params])
+        res = derivative(Fit_Functions_L2.fit_func_B_wo, params[4], args = [params])
         result += np.square(res.df[0] * u_params[4])
         
-        res = derivative(Fit_Functions_L2.fit_func_C, params[5], args = [params])
-        result += np.square(res.df[0] * u_params[5])
-        
         return np.sqrt(result)
+    """
+    @staticmethod
+    def monte_carlo(func, constants, params, cov_matrix):
+        num_run = 1000
+        for _ in range(num_run):
+            para = []
+            for i in params:
+    """
+            
 
     @staticmethod
-    def eval_L2(frequency:list, amplitude:list, axs, uncertainty_frequency, uncertainty_amplitude, beta0, test_phase = False):
-        odr_model = odr.Model(Fit_Functions_L2.fit_func_odr)
+    def eval_L2(frequency:list, amplitude:list, axs, uncertainty_frequency, uncertainty_amplitude, beta0, horizontal_shift, test_phase = False):
+        match horizontal_shift:
+            case True:
+                func = Fit_Functions_L2.fit_func_odr_with_shift
+            case False:
+                func = Fit_Functions_L2.fit_func_odr_wo_shift
+        odr_model = odr.Model(func)
         data = odr.RealData(frequency, amplitude, sx = uncertainty_frequency, sy = uncertainty_amplitude)
-        odr_fit = odr.ODR(data,odr_model, beta0 = beta0, maxit = 100)
+        odr_fit = odr.ODR(data,odr_model, beta0 = beta0, maxit = 10000)
         output = odr_fit.run()
 
         if test_phase == True:
             output.pprint()
 
-        a, omega_0, delta, B, C = output.beta[0], output.beta[1], output.beta[2], output.beta[3], output.beta[4]
-        u_a, u_omega_0, u_delta, u_B, u_C = output.sd_beta[0], output.sd_beta[1], output.sd_beta[2], output.sd_beta[3], output.sd_beta[4]
+        u_omega_0 = output.sd_beta[1]
 
         x = np.linspace(min(frequency), max(frequency), 10000)
-        y = [Fit_Functions_L2.fit_func_odr([a, omega_0, delta, B, C], i) for i in x]
+        y = [func(output.beta, i) for i in x]
         axs.plot(x,y, label = f"ODR Fit", c = "cyan")
     
         peaks, props = find_peaks(y)
         resonance_frequency = x[peaks[0]]
 
         u_A_params = np.concatenate(([resonance_frequency],output.beta))
+        u_A_unc_params = np.concatenate(([u_omega_0], output.sd_beta))
 
-        amplitude_res_freq = Fit_Functions_L2.fit_func_odr([a, omega_0, delta, B, C], resonance_frequency)
-        u_amplitude_res_freq = evaluation.u_A(u_A_params, [u_omega_0, u_a, u_omega_0, u_delta, u_B, u_C])
+        amplitude_res_freq = func(output.beta, resonance_frequency)
+        u_amplitude_res_freq = evaluation.u_A(u_A_params, u_A_unc_params, horizontal_shift)
 
         if test_phase == False:
             axs.errorbar(resonance_frequency, amplitude_res_freq, xerr = u_omega_0, yerr = u_amplitude_res_freq, label = r"Resonanzfrequenz $\omega_0$",c = "blue", fmt = "o", capsize = 3)
@@ -116,16 +182,15 @@ class evaluation():
         print(rf"Amplitude at res freq: {amplitude_res_freq} {u'\u00b1'} {u_amplitude_res_freq} with rel u. {rel_u_amp_res_freq}")
         print(rf"Amplitude at cut off freq: {amplitude_cut_off_freq} {u'\u00b1'} {u_amplitude_cut_off_freq} with rel u. {rel_u_amp_cut_freq}")
         
-        args = (a, omega_0, delta, B, C)
-        func = lambda omega, a, omega_0, delta, B, C: Fit_Functions_L2.fit_func_odr([a, omega_0, delta, B, C], omega) - amplitude_cut_off_freq
-        cut_off_freq_0 = least_squares(func, x0 = 2.5, bounds = (0, resonance_frequency), args = args)["x"][0]
-        cut_off_freq_1 = least_squares(func, x0 = resonance_frequency+2, bounds = (resonance_frequency, np.inf), args = args)["x"][0]
+        function_ls = lambda omega: func(output.beta, omega) - amplitude_cut_off_freq
+        cut_off_freq_0 = least_squares(function_ls, x0 = 1, bounds = (0, resonance_frequency))["x"][0]
+        cut_off_freq_1 = least_squares(function_ls, x0 = resonance_frequency + 2, bounds = (resonance_frequency, np.inf))["x"][0]
 
         print(f"The cut off frequencies are: {cut_off_freq_0} and {cut_off_freq_1}")
 
         if test_phase == False:
-            axs.errorbar(cut_off_freq_0, amplitude_cut_off_freq, xerr = u_omega_0, yerr = u_amplitude_cut_off_freq, label = r"Linke Grenzfrequenz $f_{g,1}$", c = "green", fmt = "o", capsize = 3)
-            axs.errorbar(cut_off_freq_1, amplitude_cut_off_freq, xerr = u_omega_0, yerr = u_amplitude_cut_off_freq, label = r"Rechte Grenzfrequenz $f_{g,2}$", c = "purple", fmt = "o", capsize = 3)
+            axs.errorbar(cut_off_freq_0, amplitude_cut_off_freq, xerr = output.sd_beta[1], yerr = u_amplitude_cut_off_freq, label = r"Linke Grenzfrequenz $f_{g,1}$", c = "green", fmt = "o", capsize = 3)
+            axs.errorbar(cut_off_freq_1, amplitude_cut_off_freq, xerr = output.sd_beta[1], yerr = u_amplitude_cut_off_freq, label = r"Rechte Grenzfrequenz $f_{g,2}$", c = "purple", fmt = "o", capsize = 3)
 
         bandwidth = cut_off_freq_1 - cut_off_freq_0
         u_bandwidth = np.sqrt(2 * np.square(u_omega_0))
